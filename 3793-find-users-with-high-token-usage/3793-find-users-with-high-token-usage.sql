0606f17d372471cc3ef2e90cbf8372ae7eea640e
Nakul Pradip Indurkar <nakulindurkar3@gmail.com>
2026-07-07 02:11:12 +0530

    Time: 285 ms (90.65%), Space: 0B (100%) - LeetHub

---
+++ 3793-find-users-with-high-token-usage/3793-find-users-with-high-token-usage.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT user_id, COUNT(user_id) prompt_count, ROUND(AVG(tokens),2) AS avg_tokens
+FROM prompts
+GROUP BY user_id
+HAVING COUNT(user_id) >= 3 AND MAX(tokens) > AVG(tokens)
+ORDER BY avg_tokens DESC, user_id
